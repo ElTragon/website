827b641e4992863dc59bb282870388df918db38e
Mario Lopez <mariodolt@marios-mbp.lan>
2023-07-03 22:07:50 -0700

    better;

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -2,21 +2,34 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 const css = require("./index.module.css")
 
-const technologies = [
-  "Javascript",
-  "Typescript",
-  "React",
-  "NextJs",
-  "Gastby",
-  "Node.Js",
-]
+const leftTech = ["Javascript", "Typescript", "React"]
+
+const rightTech = ["NextJs", "Gastby", "Node.Js"]
+
+type Props = {
+  technologies: string[]
+}
+
+const ListTech = ({ technologies }: Props) => {
+  return (
+    <ul className={css.techContainer}>
+      {technologies.map(tech => {
+        return (
+          <li>
+            <span className={css.tech}>{tech}</span>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
 
 const AboutMe = () => {
   return (
     <div className={css.container}>
-      <h2>About Me</h2>
+      <h3 className={css.header}>About Me</h3>
 
-      <p>
+      <p className={css.text}>
         Hello! I'm Mario Lopez, a software engineer from California. With
         experience at startups, database, pharmaceutical, and fintech companies,
         I've honed my skills in React, TypeScript, Go, and more. I'm passionate
@@ -24,11 +37,10 @@ const AboutMe = () => {
         collaborate on exciting projects!
       </p>
 
-      <ul>
-        {technologies.map(tech => {
-          return <li>{tech}</li>
-        })}
-      </ul>
+      <div className={css.listContainer}>
+        <ListTech technologies={leftTech} />
+        <ListTech technologies={rightTech} />
+      </div>
     </div>
   )
 }
