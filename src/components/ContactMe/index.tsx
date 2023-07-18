3bd13c50e1c5e62e130d4e57b1b918910f446728
Mario L <m9lopez@ucsd.edu>
2023-07-17 18:57:04 -0700

    closer

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -22,25 +22,25 @@ export default function ContactMe() {
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              className={css.inputEmail}
               placeholder="someone@hotmail.com"
               required
             />
           </div>
           <label
             htmlFor="message"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
+            className={css.label}
+            >
             Your message:
           </label>
           <textarea
             id="message"
             rows={4}
             cols={25}
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            className={css.inputMessage}
             placeholder="Leave a message.."
           />
-          <button>Send</button>
+          <button className={css.sendButton}>Send</button>
         </form>
     </>
   )
