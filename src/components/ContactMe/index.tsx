335171619f4d92a28ac6f1dd689abe0e8cadb329
Mario L <m9lopez@ucsd.edu>
2023-07-18 19:48:44 -0700

    style done

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 const css = require("./index.module.css")
 
-
 export default function ContactMe() {
   const [viewModule, setViewModule] = useState<boolean>(false)
 
@@ -10,10 +9,9 @@ export default function ContactMe() {
       <h3 className={css.contactMe}>Contact Me</h3>
 
       <div className={css.text}>
-        My inbox is always open. whether you have a question or just want to say
+        My inbox is always open. Whether you have a question or just want to say
         hi. I'll try my best to get back to you.
       </div>
-
 
       <form
         name="contact-form"
