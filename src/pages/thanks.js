5dde480449e427b0b8bd539670ec389e9ef927d6
Mario L <m9lopez@ucsd.edu>
2023-07-18 21:25:00 -0700

    better

---
+++ src/pages/thanks.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <h1>Thank you</h1>
+      <p>Your message has been sent.</p>
+      <a href={'/'} target="_blank" >
+        Go back to the homepage
+      </a>
+
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="Thank you" />
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
